eaadd29b3172c31bd3d2518a80f0368a697e748a
LGustavo <lgustavoccarvalho@gmail.com>
2021-10-11 10:09:35 -0300

    CriandoCenarios

--- src/test/java/tests/SetupTest.java
+++ src/test/java/tests/SetupTest.java
@@ -1,6 +1,7 @@
 package tests;
 
 import org.junit.Test;
+import org.openqa.selenium.By;
 import utils.Browser;
 import utils.Utils;
 
@@ -12,5 +13,33 @@ public class SetupTest extends BaseTests{
     public void testOpeningBrowserAndLoadingPage() {
         assertTrue(Browser.getCurrentDriver().getCurrentUrl().contains(Utils.getBaseUrl()));
         System.out.println("Abrimos o navegador e carregamos a url!");
+    }
+
+    @Test
+    public void testLogin() {
+        //buscando o botão sign in para clica-lo. Busca pela classe do botão
+        Browser.getCurrentDriver().findElement(By.className("login")).click();
+        System.out.println("Clicou Sign in e direcionou para a página de login");
+        //certificando de que está entrando na página de login
+        assertTrue(Browser.getCurrentDriver().getCurrentUrl()
+                .contains(Utils.getBaseUrl().concat("index.php?controller=authentication&back=my-account")));
+        //preenchendo o campo e-mail
+        Browser.getCurrentDriver().findElement(By.id("email")).sendKeys("rrsetcwi+autopractice@gmail.com");
+        System.out.println("Preencheu o e-mail");
+        //preenchendo o campo senha
+        Browser.getCurrentDriver().findElement(By.id("passwd")).sendKeys("teste123");
+        System.out.println("Preencheu a senha");
+        //clicando no botão de sign in
+        Browser.getCurrentDriver().findElement(By.id("SubmitLogin")).click();
+        System.out.println("Clicou em Sign in");
+        //certificando de que está na página MY ACCOUNT
+        assertTrue(Browser.getCurrentDriver().getCurrentUrl()
+                .contains(Utils.getBaseUrl().concat("index.php?controller=my-account")));
+        System.out.println("Validou a URL de MY ACCOUNT");
+        //Cerificando de que está na página correta?
+        assertTrue(Browser.getCurrentDriver().findElement(By.className("page-heading"))
+                .getText().contains("MY ACCOUNT"));
+        System.out.println("Validou Minha Conta no site");
+
     }
 }
